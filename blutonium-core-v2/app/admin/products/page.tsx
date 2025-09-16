d0ad116a88e48ada8aaf5221212655212ffd68ac
blutonium-de <d.adamiak@blutonium.de>
2025-09-17 00:15:37 +0200

    Update page.tsx

--- blutonium-core-v2/app/admin/products/page.tsx
+++ blutonium-core-v2/app/admin/products/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { useState } from 'react'
+import NavBar from "@/components/NavBar"
+
 
 type Img = { file?:File; url?:string; alt?:string }
 export default function AdminProducts(){
